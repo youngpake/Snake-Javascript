288a334fd50d63a9a95597ab63aa8efaf448a8f7
Wytse Punter <wytsep@live.nl>
2022-10-25 17:44:51 +0200

    Final

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
 
@@ -6,6 +6,25 @@ let lastRenderTime = 0
 let gameOver = false
 let isPaused = false
 const gameBoard = document.getElementById('game-board')
+let SNAKE_SPEED = 5
+
+window.increase_speed = function () {
+  SNAKE_SPEED++
+  console.log(SNAKE_SPEED)
+  document.getElementById("SNAKE_SPEED").innerHTML = SNAKE_SPEED;
+}
+window.decrease_speed = function () {
+  if (SNAKE_SPEED == 1){
+    console.log("Minimum speed is 1")
+    return
+  }
+  SNAKE_SPEED--
+  console.log(SNAKE_SPEED)
+  document.getElementById("SNAKE_SPEED").innerHTML = SNAKE_SPEED;
+}
+
+
+
 
 function main(currentTime) {
   if (gameOver) {
@@ -14,7 +33,7 @@ function main(currentTime) {
     }
     return
   }
-  
+
   if (isPaused == true)
   {return}
   window.requestAnimationFrame(main) /* do another loop */
@@ -27,13 +46,9 @@ function main(currentTime) {
   draw()
 }
 
-if (isPaused == false){
-  window.requestAnimationFrame(main)
-}
-
 window.addEventListener('keydown', e => {
   switch (e.key) {
-    case 'p':
+    case 'Escape':
       if (isPaused == true){ /* switch to live game */
         isPaused = false
         console.log("running")
@@ -69,3 +84,4 @@ function draw() {
 function checkDeath() {
   gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
+
--- snake.js
+++ snake.js
@@ -1,6 +1,6 @@
 import { getInputDirection } from "./input.js"
 
-export const SNAKE_SPEED = 5
+/*export var SNAKE_SPEED = 5*/
 const snakeBody = [{ x: 11, y: 11 }]
 let newSegments = 0
 
